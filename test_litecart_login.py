ba52816647e43ad23583d68a2f94b75ccd65b3af
Adept-Work <dmitry.potapov@gs-labs.ru>
2017-07-19 08:14:44 +0200

    litecart first lesson

---
+++ test_litecart_login.py
@@ -0,0 +1,25 @@
+from time import sleep
+import pytest
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+
+
+@pytest.fixture
+def driver(request):
+    web_driver = webdriver.Chrome()
+    web_driver.maximize_window()
+    web_driver.implicitly_wait(5)
+    request.addfinalizer(web_driver.quit)
+    return web_driver
+
+
+def test_example(driver):
+    driver.get("http://barancev.i-adept.ru/adept/login.php")
+    sleep(2)
+    driver.find_element_by_name('username').click
+    driver.find_element_by_name('username').send_keys('adept2')
+    driver.find_element_by_name('password').click
+    driver.find_element_by_name('password').send_keys('zmowv53479353jtzv')
+    driver.find_element_by_name('login').click()
+    sleep(2)
+    driver.quit()
